b9ceb04bf00060b3733a042cab04ad4c2ac7ae78
yangcn <yangchunning@senyint.com>
2018-12-18 16:20:32 +0800

    第一次

---
+++ src/HelloApplication.java
@@ -0,0 +1,87 @@
+import org.eclipse.swt.widgets.Display;
+import org.eclipse.swt.widgets.Shell;
+import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.SWT;
+import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.FormLayout;
+import org.eclipse.swt.layout.FormData;
+import org.eclipse.swt.layout.FormAttachment;
+import org.eclipse.swt.widgets.Label;
+import org.eclipse.swt.widgets.Text;
+import org.eclipse.swt.layout.GridData;
+import org.eclipse.swt.widgets.Button;
+
+
+public class HelloApplication {
+	private static Text text;
+	private static Text text_1;
+
+	/**
+	 * Launch the application.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		Display display = Display.getDefault();
+		Shell shell = new Shell();
+		shell.setSize(450, 300);
+		shell.setText("SWT Application");
+		shell.setLayout(new FormLayout());
+		
+		Composite composite = new Composite(shell, SWT.NONE);
+		FormData fd_composite = new FormData();
+		fd_composite.bottom = new FormAttachment(0, 188);
+		fd_composite.right = new FormAttachment(0, 327);
+		fd_composite.top = new FormAttachment(0, 22);
+		fd_composite.left = new FormAttachment(0, 41);
+		composite.setLayoutData(fd_composite);
+		composite.setLayout(new GridLayout(5, false));
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		
+		Label label = new Label(composite, SWT.NONE);
+		label.setText("用户名：");
+		new Label(composite, SWT.NONE);
+		
+		text = new Text(composite, SWT.BORDER);
+		text.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		
+		Label label_1 = new Label(composite, SWT.NONE);
+		label_1.setText("密码：");
+		new Label(composite, SWT.NONE);
+		
+		text_1 = new Text(composite, SWT.BORDER);
+		text_1.setLayoutData(new GridData(SWT.FILL, SWT.CENTER, true, false, 2, 1));
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		
+		Button button = new Button(composite, SWT.NONE);
+		button.setText("登录");
+		new Label(composite, SWT.NONE);
+		new Label(composite, SWT.NONE);
+		
+		Button button_1 = new Button(composite, SWT.NONE);
+		button_1.setText("取消");
+
+		shell.open();
+		shell.layout();
+		while (!shell.isDisposed()) {
+			if (!display.readAndDispatch()) {
+				display.sleep();
+			}
+		}
+	}
+}
