8685d01f61b93e5b5b2960e2ff2ae8b2a6615e10
yangcn <yangchunning@senyint.com>
2019-01-30 15:11:42 +0800

    1.初始化

---
+++ src/main/java/com/yangcn/HelloGit/App.java
@@ -0,0 +1,13 @@
+package com.yangcn.HelloGit;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
